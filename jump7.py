21bbf1b71e21fa3248cda96f07f81d2494863c16
WaitOnRoad <330766075@qq.com>
2020-03-23 20:28:48 +0800

    jump7.py

---
+++ jump7.py
@@ -0,0 +1,11 @@
+#git xuexi
+#git init  chushihua cangku
+#git status chakan cangku zhuangtai
+#chuangjian yige wenjian
+#git config --global user.name "WaitOnRoad"  lianjie ziji de git cangku nicheng
+#git config --global user.email "330766075@qq.com" lianjie ziji de cangku email
+#git remote add origin url  tianjia yuanchengcangku jiaogei  github guanli 
+#git add --all  tianjia suoyou wenjian
+#git commit -m "tijiao miaoshu"
+#git push origin master
+#git clone url clone duiying de cangku dao bendi
